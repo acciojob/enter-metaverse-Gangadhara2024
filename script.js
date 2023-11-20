353b3e5ad16aeca61ae22e4dd80db196ff2582d1
Ruttala Gangadhara rao <136541746+Gangadhara2024@users.noreply.github.com>
2023-11-20 12:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const para = document.getElementById("status");
+
+function enterText() {
+	const newElement = document.createElement("p");
+	newElement.innerText = "Entered Metaverse";
+	para.appendChild(newElement);
+}
